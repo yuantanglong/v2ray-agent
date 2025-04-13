2557763e58081d1bc3f49d9c4d59981f29a5966e
yuantanglong <tanglong.yuan@zhaopin.com.cn>
2025-04-13 21:14:09 +0800

    添加alpn

--- install.sh
+++ install.sh
@@ -4408,6 +4408,10 @@ EOF
             "tlsSettings": {
               "rejectUnknownSni": true,
               "minVersion": "1.2",
+              "alpn": [
+                "http/1.1",
+                "h2"
+              ],
               "certificates": [
                 {
                   "certificateFile": "/etc/v2ray-agent/tls/${domain}.crt",
